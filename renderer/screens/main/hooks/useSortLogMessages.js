2407978ef68e8ec7f82185b62ce74bdcc0a9efab
Daniel Destouche <daniel.destouche@gmail.com>
2025-10-15 13:37:27 -0400

    fix: Sorting method was not properly sorting fields

--- renderer/screens/main/hooks/useSortLogMessages.js
+++ renderer/screens/main/hooks/useSortLogMessages.js
@@ -1,5 +1,16 @@
 import { useState, useEffect } from "../../../core/index.js";
 
+const StringCollator = new Intl.Collator("en", {
+  numeric: true,
+  sensitivity: "base",
+});
+
+const sortStringAscending = (a, b) => StringCollator.compare(a, b);
+const sortStringDescending = (a, b) => StringCollator.compare(b, a);
+
+const sortNumberAscending = (a, b) => a - b;
+const sortNumberDescending = (a, b) => b - a;
+
 /**
  * A custom hook to sort log messages by a given field and order
  * @param {import('../main/network/proxyapi.js').LogMessage[]} logMessages the log messages to sort
@@ -14,11 +25,23 @@ export const useSortLogMessages = (logMessages, sortBy, order) => {
   const [sortedMessages, setSortedMessages] = useState([]);
 
   useEffect(() => {
-    const sorted = [...logMessages].sort((a, b) => {
-      if (a[sortBy] < b[sortBy]) return order === "asc" ? -1 : 1;
-      if (a[sortBy] > b[sortBy]) return 1;
-      return 0;
-    });
+    const message = logMessages.find((msg) => msg[sortBy] !== undefined);
+    const fieldType = message ? typeof message[sortBy] : "string";
+
+    let sorted = [];
+    if (fieldType === "string") {
+      sorted = [...logMessages].sort((a, b) =>
+        order === "asc"
+          ? sortStringAscending(a[sortBy], b[sortBy])
+          : sortStringDescending(a[sortBy], b[sortBy])
+      );
+    } else {
+      sorted = [...logMessages].sort((a, b) =>
+        order === "asc"
+          ? sortNumberAscending(a[sortBy], b[sortBy])
+          : sortNumberDescending(a[sortBy], b[sortBy])
+      );
+    }
 
     setSortedMessages(sorted);
   }, [logMessages, sortBy, order]);
